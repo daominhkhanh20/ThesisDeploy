28ee4d184cb422dd79fcebbc2a720afde3d1e24c
daominhkhanh20 <khanhc1k36@gmail.com>
2023-06-12 17:38:25 +0700

    update api

--- backend/main.py
+++ backend/main.py
@@ -1,30 +1,34 @@
-import tritonclient 
-from tritonclient.utils import np_to_triton_dtype
-import tritonhttpclient
 import typing 
 from fastapi import FastAPI
 import numpy as np 
+import tritonclient 
+from tritonclient.utils import np_to_triton_dtype
+import tritonhttpclient
 
 app = FastAPI()
-INPUT_NAME = 'query'
+INPUT_NAMES = ['query']
 OUTPUT_NAMES = ['e2e_answer']
 MODEL_NAME = 'ensemble_model'
 URL_TRITON = '0.0.0.0:8000'
-MODEL_VERSION = '-1'
+MODEL_VERSION = '1'
 VERBOSE = 1
 
 triton_client = tritonhttpclient.InferenceServerClient(
-    url=URL_TRITON,
-    verbose=VERBOSE
-)
+        url=URL_TRITON,
+        verbose=VERBOSE
+    )
 
-@app.post('/answer')
+@app.get("/")
+async def root():
+    return {"message": "Hello wordl"}
+
+@app.get('/answer')
 def answer_question(sentence: str):
-    input_feature = np.array(
-        bytes(sentence, 'utf-8'), dtype=np.bytes_
-    ).reshape(1, 1)
+    print(sentence)
+    input_feature = np.array([bytes(sentence, 'utf8')], dtype=np.bytes_).reshape(1, 1)
     
-    input0 = tritonhttpclient.InferInput(INPUT_NAME, input_feature.shape, np_to_triton_dtype(input_feature.dtype))
+    input0 = tritonhttpclient.InferInput(INPUT_NAMES[0], input_feature.shape, np_to_triton_dtype(input_feature.dtype))
+    input0.set_data_from_numpy(input_feature)
     list_output = []
     for out in OUTPUT_NAMES:
         list_output.append(
@@ -32,5 +36,6 @@ def answer_question(sentence: str):
                 out, binary_data=False
             )
         )
+    print(input_feature)
     response = triton_client.infer(model_name=MODEL_NAME, model_version=MODEL_VERSION, inputs=[input0], outputs=list_output)
-    return {'answer': response.as_numpy('e2e_answer')}
+    return {'answer': response.as_numpy('e2e_answer')[0][0]}
