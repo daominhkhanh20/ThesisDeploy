f31409a17f0e1c857acbe8b9492e2f030f8b2655
daominhkhanh20 <khanhc1k36@gmail.com>
2023-07-23 18:01:43 +0700

    update

--- backend/scripts/download_model.sh
+++ backend/scripts/download_model.sh
@@ -3,6 +3,8 @@ export KAGGLE_USERNAME='daominhkhanh'
 export KAGGLE_KEY="53d2021e6812290870fc3520cbeee5ea"
 kaggle datasets download -d daominhkhanh/model-compile-v$VERSION
 unzip model-compile-v$VERSION.zip
+mkdir model/sbert_retrieval/1
+mkdir model/qa_model/1
 mv sbert/model.pt model/sbert_retrieval/1
 mv qa/model.pt model/qa_model/1
 mv corpus.json model/corpus.json
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const SearchBar = ({ handleSearch }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    handleSearch(searchTerm);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={searchTerm}
+        onChange={handleChange}
+        placeholder="Search..."
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
+};
+
+const SearchResult = ({ results }) => {
+  return (
+    <div className="result-container">
+      {results.map((result) => (
+        <div key={result.id} className="result-item">
+          <a href={result.link}>{result.title}</a>
+          <p>{result.description}</p>
+        </div>
+      ))}
     </div>
   );
-}
+};
+
+const App = () => {
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleSearch = (searchTerm) => {
+    // Simulated API call or search logic
+    const results = [
+      { id: 1, title: 'Result 1', description: 'Description for Result 1', link: '#' },
+      { id: 2, title: 'Result 2', description: 'Description for Result 2', link: '#' },
+      { id: 3, title: 'Result 3', description: 'Description for Result 3', link: '#' },
+    ];
+
+    setSearchResults(results);
+  };
+
+  return (
+    <div className="app">
+      <SearchBar handleSearch={handleSearch} />
+      <SearchResult results={searchResults} />
+    </div>
+  );
+};
 
 export default App;
